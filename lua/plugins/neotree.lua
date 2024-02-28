f298494363f20555519052f6de0a84b7ee79c459
Thadryan <thadryan@protonmail.com>
2024-02-28 07:44:54 -0500

    window width

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,24 +8,17 @@ return {
             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
-        opts = {
-            filesystem = {
-                filtered_items = {
-                    visible = true,
-                    show_hidden_count = true,
-                    hide_dotfiles = false,
-                    hide_gitignored = true,
-                    hide_by_name = {
-                        -- '.git',
-                        -- '.DS_Store',
-                        -- 'thumbs.db',
-                    },
-                    never_show = {},
-                },
-            }
-        },
         config = function()
-            nmap('<c-b>', ':Neotree toggle dir=%:p:h<cr>')
+
+            nmap('<c-b>', ':Neotree toggle dir=%:p:h<CR>')
+            nmap('<leader>nt', ':Neotree <CR>')
+
+            require("neo-tree").setup({
+                window = {
+                    width = 27
+                }
+            })
+
         end
     }
 }
