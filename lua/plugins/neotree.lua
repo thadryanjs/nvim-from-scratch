16c900a142cf4c83fa6013e070a439e69fd9fa6e
Thadryan <thadryan@protonmail.com>
2024-02-28 07:50:12 -0500

    optimal neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,9 +9,9 @@ return {
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
         config = function()
-
-            nmap('<c-b>', ':Neotree toggle dir=%:p:h<CR>')
-            nmap('<leader>nt', ':Neotree <CR>')
+            -- this sets the working dir - could be useful so I'm leaving it but not using it for now
+--            nmap('<c-b>', ':Neotree toggle dir=%:p:h<CR>')
+            nmap('<leader>nt', ':Neotree toggle<CR>')
 
             require("neo-tree").setup({
                 window = {
