a6d023c0d5bb492c9c3321369efac7c4fdc0a816
Thadryan <thadryan@protonmail.com>
2024-06-14 13:23:40 -0400

    mark hotkeys

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -8,6 +8,9 @@ return {
             vim.api.nvim_set_keymap('n', '<leader>mn', "<cmd>lua require('marks').next_mark()<CR>", { noremap = true, silent = true })
             vim.api.nvim_set_keymap('n', '<leader>mp', "<cmd>lua require('marks').prev_mark()<CR>", { noremap = true, silent = true })
             vim.api.nvim_set_keymap('n', '<leader>mf', ":Telescope marks <CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>mt', ":MarksToggleSigns <CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>ma', ":MarksListAll <CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>mb', ":MarksListBuf <CR>", { noremap = true, silent = true })
 
         end
     }
